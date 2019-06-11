59d60dcf23c5efb8acec5577ef06512189edfe7e
Emmanuel Turbet <m.emmanuel.td@gmail.com>
2019-06-11 10:44:48 +0200

    refacto

--- p_classification.adb
+++ p_classification.adb
@@ -1,6 +1,6 @@
 package body P_Classification is
    
    function Score(D: in TR_Depeche; L : in TV_Lexique) return Integer;
-   --{ } => { Résultat = Score De La Dépêche D Pour La Catégorie Dont Le Lexique Est L}
+   -- {} => { Résultat = Score De La Dépêche D Pour La Catégorie Dont Le Lexique Est L}
     
 end P_classification
