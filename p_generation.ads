995f09f0948c05a7498f5563e3918284d5a97908
elmimyas <yassine.elmimouni@outlook.com>
2019-06-11 21:41:39 +0200

    ajout des fct de 3.1 et 3.4

--- p_generation.ads
+++ p_generation.ads
@@ -1,3 +1,19 @@
 package p_generation is
-        
+   
+   type TR_Entree is record
+      Mot: String(1..30);
+      Score: Integer;
+   end record;
+   
+   type TV_Dico is array(1..2000) of TR_Entree;
+   
+   function Recherche(VM: in Tv_Dico; N: in Integer; M: in String) return Integer;
+   -- {} => {Resultat= Indice Du Mot M Dans Le Vecteur VM Si Il Est Présent Et -1 Sinon. N Est Le Nombre De Mots Rangés Dans Le vecteur}
+     
+   procedure Init_Dico(VD: in Tv_Depeche; C: in T_Categorie; VM: out Tv_Dico; N: out Integer);
+   -- {} => {Charge Dans VM Tous Les Mots Présents Dans Au Moins Une Dépêche De La Catégorie C Du Vecteur De Dépêches VD. Attention, Même Si Le Mot Est Présent Plusieurs Fois, Il Ne Doit Apparaître Qu'Une Fois Dans Le Vecteur VM. La Procédure Initialise Aussi Tous Les Scores À 0 Et range Dans N Le Nombre De Mots Ajoutés Dans VM }
+     
+   procedure Generation_Lexique (VD: in Tv_Depeche; C : in T_Categorie; Fl: in String);
+   -- {} => {Cette Procédure Créé Pour La Catégorie C Le Fichier Lexique De Nom Fl À Partir Du Vecteur De Dépêches De Nom VD. Cette Procédure Doit Déclarer Un Vecteur De type TV_Dico Puis Le Remplir En Utilisant Init_Dico, Puis Calcul_Scores Et Enfin Utiliser Le Vecteur Résultant Pour Créer Un Fichier Lexique En Utilisant La Fonction Poids_Score}
+     
 end p_generation;
