a0ef385d61440071b2f8327c2db54795bd6e2f29
Emmanuel Turbet <m.emmanuel.td@gmail.com>
2019-06-11 10:43:16 +0200

    ajout max_score p_classification.ads

--- p_classification.ads
+++ p_classification.ads
@@ -1,6 +1,9 @@
 package P_classification is
    
    function Score(D: in TR_Depeche; L : in TV_Lexique) return Integer;
-   { } => { Résultat = Score De La Dépêche D Pour La Catégorie Dont Le Lexique Est L}
-      
+   -- {} => { Résultat = Score De La Dépêche D Pour La Catégorie Dont Le Lexique Est L}
+     
+   function Max_Score(VS : in TV_Score) return T_Categorie;
+   -- {} => {resultat = Catégorie ayant le score maximal dans VS}
+     
 end p_classification;
