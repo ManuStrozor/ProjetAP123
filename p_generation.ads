f12e2540767995eb75fbdd29d727bbb4fbf851c8
Emmanuel Turbet <m.emmanuel.td@gmail.com>
2019-06-12 00:43:41 +0200

    fonctions pas encore faites en comm + ajout with/use P_depeche

--- p_generation.ads
+++ p_generation.ads
@@ -1,3 +1,5 @@
+with P_Depeche; use P_Depeche;
+
 package p_generation is
    
    type TR_Entree is record
@@ -13,10 +15,13 @@ package p_generation is
    procedure Init_Dico(VD: in Tv_Depeche; C: in T_Categorie; VM: out Tv_Dico; N: out Integer);
    -- {} => {Charge Dans VM Tous Les Mots Présents Dans Au Moins Une Dépêche De La Catégorie C Du Vecteur De Dépêches VD. Attention, Même Si Le Mot Est Présent Plusieurs Fois, Il Ne Doit Apparaître Qu'Une Fois Dans Le Vecteur VM. La Procédure Initialise Aussi Tous Les Scores À 0 Et range Dans N Le Nombre De Mots Ajoutés Dans VM }
    
-   procedure Generation_Lexique (VD: in Tv_Depeche; C : in T_Categorie; Fl: in String);
-   -- {} => {Cette Procédure Créé Pour La Catégorie C Le Fichier Lexique De Nom Fl À Partir Du Vecteur De Dépêches De Nom VD. Cette Procédure Doit Déclarer Un Vecteur De type TV_Dico Puis Le Remplir En Utilisant Init_Dico, Puis Calcul_Scores Et Enfin Utiliser Le Vecteur Résultant Pour Créer Un Fichier Lexique En Utilisant La Fonction Poids_Score}
-   
    procedure Calcul_Scores(VD: in Tv_Depeche;C: in T_Categorie; VM: in out Tv_Dico;N: in Integer);
    -- {} => {Cette procédure met à jour les scores des différents mots présents dans VM. Lorsqu'un mot présent dans VM apparaît dans une dépêche du vecteur VD, son score est décrémenté si la dépêche n'est pas dans la catégorie C et incrémenté si la dépêche est dans la catégorie C}
+   
+   --function Poids_Score(S : in Integer) return Integer;
+   -- {} => {resultat = valeur du poids à attribuer étant donné un score S}
+   
+   --procedure Generation_Lexique(VD: in Tv_Depeche; C : in T_Categorie; Fl: in String);
+   -- {} => {Cette Procédure Créé Pour La Catégorie C Le Fichier Lexique De Nom Fl À Partir Du Vecteur De Dépêches De Nom VD. Cette Procédure Doit Déclarer Un Vecteur De type TV_Dico Puis Le Remplir En Utilisant Init_Dico, Puis Calcul_Scores Et Enfin Utiliser Le Vecteur Résultant Pour Créer Un Fichier Lexique En Utilisant La Fonction Poids_Score}
    
 end p_generation;
