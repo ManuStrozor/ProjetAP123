a5fe0a5ce170759d30373c204cd7b53f041d71c5
elmimyas <yassine.elmimouni@outlook.com>
2019-06-11 10:40:45 +0200

    ajout p_classification.ads avec fct score

--- p_classification.ads
+++ p_classification.ads
@@ -1,4 +1,6 @@
 package P_classification is
-        
-     
+   
+   function Score(D: in TR_Depeche; L : in TV_Lexique) return Integer;
+   { } => { Résultat = Score De La Dépêche D Pour La Catégorie Dont Le Lexique Est L}
+      
 end p_classification;
