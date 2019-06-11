4d00993c3f9b9b69b5930bb1555e851cf7fd6ac4
Emmanuel Turbet <m.emmanuel.td@gmail.com>
2019-06-12 00:41:58 +0200

    test Init_dico & Recherche...OK

---
+++ p6.adb
@@ -0,0 +1,22 @@
+with P_Generation; use P_Generation;
+with P_Classification; use P_Classification;
+with P_Depeche; use P_Depeche;
+with P_Lexique; use P_Lexique;
+with Text_Io; use Text_Io;
+
+procedure P6 is
+   TVD : TV_Depeche;
+   Dico : TV_Dico;
+   Nb : Integer := 0;
+begin
+   Charge("depeches.txt", TVD);
+   declare
+      Ls : TV_Lexique(1..Nb_Mots("SPORTS.txt"));
+   begin
+      Init_Lexique("SPORTS.txt", Ls);
+      Init_Dico(TVD, Sports, Dico, Nb);
+      for I in Dico'First..Nb loop
+	 Put_Line(Integer'Image(I) & ' ' & Dico(I).Mot &':'& Integer'Image(Dico(I).Score));
+      end loop;
+   end;
+end;
