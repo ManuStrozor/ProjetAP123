6eee18cbe43ad9ee9b0ce6d01f46fdff442fea71
Emmanuel Turbet <m.emmanuel.td@gmail.com>
2019-06-11 14:14:07 +0200

    add 5.3 functions procedures

--- p_classification.adb
+++ p_classification.adb
@@ -2,5 +2,50 @@ package body P_Classification is
    
    function Score(D: in TR_Depeche; L : in TV_Lexique) return Integer;
    -- {} => { Résultat = Score De La Dépêche D Pour La Catégorie Dont Le Lexique Est L}
-    
+   
+   function Max_Score(VS : in TV_Score) return T_Categorie is
+      Max : Integer := VS(VS'first);
+      Cat : T_Categorie := VS'first;
+   begin
+      for I in VS'Range loop
+	 if VS(I) > Max then
+	    Max := VS(I);
+	    Cat := I;
+	 end if;
+      end loop;
+      return Cat;
+   end;
+   
+   procedure Run (VD : in TV_Depeche; Lp, Ls, Lc, Le, Lt : TV_Lexique; Nomfic : String) is
+      F : Text_Io.File_Type;
+      Tmp : TV_Lexique;
+      type TV_Cnt is array(T_Categorie) of Positive;
+      Counter : TV_Cnt := (others => 0);
+      Moy : Float := 0.0;
+   begin
+      Open(F, Out_File, Nomfic);
+      for I in VD'Range loop
+	 Put_Line(F, VD(i).Id & ':' & VD(I).Cat);
+	 
+	 if VD(I).Cat = Politique then
+	    Tmp := Lp;
+	 elsif VD(I).Cat = Sports then
+	    Tmp := Ls;
+	 elsif VD(I).Cat = Culture then
+	    Tmp := Lc;
+	 elsif VD(I).Cat = Economie then
+	    Tmp := Le;
+	 else
+	    Tmp := Lt;
+	 end if;
+	 
+	 Counter(VD(I).Cat) := Counter(VD(I).Cat) + Score(VD(I), Tmp);
+      end loop;
+      for I in T_Categorie loop
+	 Put_Line(Image(I) & ':' & Integer'Image(Counter(I)));
+	 Moy := Moy + Float(Counter(I));
+      end loop;
+      Put_Line("MOYENNE:" & Float'Image(Moy/5.0));
+   end;
+   
 end P_classification
--- p_classification.ads
+++ p_classification.ads
@@ -1,3 +1,5 @@
+with P_Depeche; use P_Depeche;
+
 package P_classification is
    
    function Score(D: in TR_Depeche; L : in TV_Lexique) return Integer;
@@ -5,5 +7,8 @@ package P_classification is
      
    function Max_Score(VS : in TV_Score) return T_Categorie;
    -- {} => {resultat = Catégorie ayant le score maximal dans VS}
-     
+   
+   procedure Run (VD : in TV_Depeche; Lp, Ls, Lc, Le, Lt : TV_Lexique; Nomfic : String);
+   -- {} => {Génère le fichier texte réponse Nomfic étant donné les dépêches et les lexiques passés en argument}
+   
 end p_classification;
