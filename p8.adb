7103009454343d1489994be3f8db606efb7e7217
Emmanuel Turbet <m.emmanuel.td@gmail.com>
2019-06-12 16:36:59 +0200

    refacto avec utrim() et trim()

--- p8.adb
+++ p8.adb
@@ -3,31 +3,26 @@ with P_Classification; use P_Classification;
 with P_Depeche; use P_Depeche;
 with P_Lexique; use P_Lexique;
 with Text_Io; use Text_Io;
+with P_String; use P_String;
 
 procedure P8 is
+   
+   type TV_Files is array(T_Categorie) of String(1..30);
+   
+   Files : TV_Files := (uTrim("POLITIQUE-2.txt", 30),
+			uTrim("SPORTS-2.txt", 30),
+			uTrim("CULTURE-2.txt", 30),
+			uTrim("ECONOMIE-2.txt", 30),
+			uTrim("ENVIRONNEMENT-SCIENCES-2.txt", 30));
+   
    TVD : TV_Depeche;
    Dico : TV_Dico;
    Nb : Integer := 0;
 begin
    Charge("depeches.txt", TVD);
-   
-   Init_Dico(TVD, Politique, Dico, Nb);
-   Calcul_Scores(TVD, Politique, Dico, Nb);
-   Generation_Lexique(TVD, Politique, "POLITIQUE-2.txt");
-   
-   Init_Dico(TVD, Sports, Dico, Nb);
-   Calcul_Scores(TVD, Sports, Dico, Nb);
-   Generation_Lexique(TVD, Sports, "SPORTS-2.txt");
-   
-   Init_Dico(TVD, Culture, Dico, Nb);
-   Calcul_Scores(TVD, Culture, Dico, Nb);
-   Generation_Lexique(TVD, Culture, "CULTURE-2.txt");
-   
-   Init_Dico(TVD, Economie, Dico, Nb);
-   Calcul_Scores(TVD, Economie, Dico, Nb);
-   Generation_Lexique(TVD, Economie, "ECONOMIE-2.txt");
-   
-   Init_Dico(TVD, Science, Dico, Nb);
-   Calcul_Scores(TVD, Science, Dico, Nb);
-   Generation_Lexique(TVD, Science, "ENVIRONNEMENT-SCIENCES-2.txt");
+   for I in Files'range loop
+      Init_Dico(TVD, I, Dico, Nb);
+      Calcul_Scores(TVD, I, Dico, Nb);
+      Generation_Lexique(TVD, I, Trim(Files(I)));
+   end loop;
 end;
