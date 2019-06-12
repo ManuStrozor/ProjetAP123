7019813e69e27980c2b035ac8823b36c800cca17
Emmanuel Turbet <m.emmanuel.td@gmail.com>
2019-06-12 16:37:36 +0200

    Ajout Infos_Score() dans p7

--- p7.adb
+++ p7.adb
@@ -2,12 +2,15 @@ with P_Generation; use P_Generation;
 with P_Classification; use P_Classification;
 with P_Depeche; use P_Depeche;
 with P_Lexique; use P_Lexique;
+with P_Esiut; use P_Esiut;
 with Text_Io; use Text_Io;
 
 procedure P7 is
    TVD : TV_Depeche;
    Dico : TV_Dico;
    Nb : Integer := 0;
+   Moy : Float;
+   Max, Min : Integer;
 begin
    Charge("depeches.txt", TVD);
    Init_Dico(TVD, Sports, Dico, Nb);
@@ -15,4 +18,9 @@ begin
    for I in Dico'First..Nb loop
       Put_Line(Integer'Image(I) & ' ' & Dico(I).Mot &':'& Integer'Image(Dico(I).Score));
    end loop;
+   A_La_Ligne;
+   Infos_Score(Dico, Nb, Moy, Max, Min);
+   Ecrire_Ligne("Moy : " & Image(Moy));
+   Ecrire_Ligne("Max : " & Image(Max));
+   Ecrire_Ligne("Min : " & Image(Min));
 end;
