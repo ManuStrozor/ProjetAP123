6cfb7b34ef523e95b162290430d4579d0b0f0bc2
Emmanuel Turbet <m.emmanuel.td@gmail.com>
2019-06-12 02:28:07 +0200

    refacto

--- p6.adb
+++ p6.adb
@@ -10,13 +10,8 @@ procedure P6 is
    Nb : Integer := 0;
 begin
    Charge("depeches.txt", TVD);
-   declare
-      Ls : TV_Lexique(1..Nb_Mots("SPORTS.txt"));
-   begin
-      Init_Lexique("SPORTS.txt", Ls);
-      Init_Dico(TVD, Sports, Dico, Nb);
-      for I in Dico'First..Nb loop
-	 Put_Line(Integer'Image(I) & ' ' & Dico(I).Mot &':'& Integer'Image(Dico(I).Score));
-      end loop;
-   end;
+   Init_Dico(TVD, Sports, Dico, Nb);
+   for I in Dico'First..Nb loop
+      Put_Line(Integer'Image(I) & ' ' & Dico(I).Mot &':'& Integer'Image(Dico(I).Score));
+   end loop;
 end;
