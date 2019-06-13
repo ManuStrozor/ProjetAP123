34dcaf4082e6c33bb6a3d84e91168504b667f2b3
Emmanuel Turbet <m.emmanuel.td@gmail.com>
2019-06-13 17:12:40 +0200

    p8 avec Calendar

--- p8.adb
+++ p8.adb
@@ -5,23 +5,39 @@ with P_Lexique; use P_Lexique;
 with Text_Io; use Text_Io;
 with P_String; use P_String;
 
+with Calendar; use Calendar;
+
 procedure P8 is
    
    type TV_Files is array(T_Categorie) of String(1..30);
    
-   F : TV_Files := (uTrim("POLITIQUE-2.txt", 30),
-			uTrim("SPORTS-2.txt", 30),
-			uTrim("CULTURE-2.txt", 30),
-			uTrim("ECONOMIE-2.txt", 30),
-			uTrim("SCIENCES-2.txt", 30));
-   
+   F : TV_Files;
    TVD : TV_Depeche;
-   Dico : TV_Dico;
-   Nb : Integer := 0;
+   
+   -- TIME --
+   Time1, Time2 : Time;
+   Annee : year_number;
+   Mois : month_number;
+   Jour : day_number;
+   Sec1, Sec2 : Day_Duration;
 begin
+   Time1 := Clock;
+   
    Charge("depeches.txt", TVD);
+   
+   F := (uTrim("POLITIQUE-2.txt", 30),
+	 uTrim("SPORTS-2.txt", 30),
+	 uTrim("CULTURE-2.txt", 30),
+	 uTrim("ECONOMIE-2.txt", 30),
+	 uTrim("SCIENCES-2.txt", 30));
    
    for I in F'range loop
       Generation_Lexique(TVD, I, Trim(F(I)));
    end loop;
+   
+   -- TIME --
+   Time2 := Clock;
+   Split (Time1, Annee, Mois, Jour, Sec1);
+   Split (Time2, Annee, Mois, Jour, Sec2);
+   Put_Line("Duree:" & Day_Duration'Image(Sec2 - Sec1));
 end;
