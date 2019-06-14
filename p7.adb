d7e4d74f27e880331eede5e1a2278b3dd9f9d185
Emmanuel Turbet <m.emmanuel.td@gmail.com>
2019-06-14 16:05:28 +0200

    test de infos_scores

--- p7.adb
+++ p7.adb
@@ -8,30 +8,25 @@ with Text_Io; use Text_Io;
 procedure P7 is   
    TVD : TV_Depeche;
    Dico : TV_Dico(1..2000);
-   Nb : Integer := 0; 
-   Inf : TR_Inf;
+   N : Integer;
+   Min, Q1, Med, Q3, Max : Integer := 0;
 begin
    Charge("depeches.txt", TVD);
    
    for I in T_Categorie loop
-      Init_Dico(TVD, I, Dico, Nb);
-      Calcul_Scores(TVD, I, Dico, Nb);
+      N := 0;
+      Init_Dico(TVD, I, Dico, N);
+      Calcul_Scores(TVD, I, Dico, N);
       
       A_La_Ligne;
       Ecrire_Ligne(Image(I) & ':');
-      Infos_Score(Dico, Nb, Inf);
-      Ecrire("Max:" & Image(Inf.Max) & "  ");
-      Ecrire("Moy:" & Image(Inf.Moy));
+      Infos_Scores(Dico, N, Min, Q1, Med, Q3, Max);
+      Ecrire("Min:" & Image(Min) & "  ");
+      Ecrire("Max:" & Image(Max));
       
       A_La_Ligne;
-      Ecrire("Q1:" & Image(Inf.Q1) & "  ");
-      Ecrire("Q2:" & Image(Inf.Q2) & "  ");
-      Ecrire("Q3:" & Image(Inf.Q3));
-      
-      A_La_Ligne;
-      Ecrire_Ligne("Nb0Q1:" & Image(Inf.Nb0Q1));
-      Ecrire_Ligne("NbQ1Q2:" & Image(Inf.NbQ1Q2));
-      Ecrire_Ligne("NbQ2Q3:" & Image(Inf.NbQ2Q3));
-      Ecrire_Ligne("NbQ3Max:" & Image(Inf.NbQ3Max));
+      Ecrire("Q1:" & Image(Q1) & "  ");
+      Ecrire("Med:" & Image(Med) & "  ");
+      Ecrire("Q3:" & Image(Q3));
    end loop;
 end;
