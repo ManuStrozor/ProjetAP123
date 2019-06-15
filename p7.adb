7ed28fd607fbb0e2e29cf99359436e780c499077
Emmanuel Turbet <m.emmanuel.td@gmail.com>
2019-06-15 11:36:41 +0200

    p7: lie une categorie et liste les mots du dico / pinfo: affiche les infos_scores de toutes les categories

--- p7.adb
+++ p7.adb
@@ -1,31 +1,24 @@
 with P_Generation; use P_Generation;
 with P_Classification; use P_Classification;
 with P_Depeche; use P_Depeche, P_Depeche.P_categorieio;
-with P_Lexique; use P_Lexique;
 with P_Esiut; use P_Esiut;
-with Text_Io; use Text_Io;
+with P_String; use P_String;
 
 procedure P7 is   
    TVD : TV_Depeche;
    Dico : TV_Dico(1..2000);
-   N : Integer;
+   N : Integer := 0;
    Min, Q1, Med, Q3, Max : Integer := 0;
+   Rep : T_Categorie;
 begin
    Charge("txt/depeches.txt", TVD);
-   for I in T_Categorie loop
-      N := 0;
-      Init_Dico(TVD, I, Dico, N);
-      Calcul_Scores(TVD, I, Dico, N);
-      
-      A_La_Ligne;
-      Ecrire_Ligne(Image(I) &':');
-      Infos_Scores(Dico, N, Min, Q1, Med, Q3, Max);
-      Ecrire("Min:" & Image(Min) & "  ");
-      Ecrire("Max:" & Image(Max));
-      
-      A_La_Ligne;
-      Ecrire("Q1:" & Image(Q1) & "  ");
-      Ecrire("Med:" & Image(Med) & "  ");
-      Ecrire("Q3:" & Image(Q3));
+   
+   Ecrire("Categorie: "); Lire(Rep);
+   
+   Init_Dico(TVD, Rep, Dico, N);
+   Calcul_Scores(TVD, Rep, Dico, N);
+   
+   for I in Dico'First..N loop
+      Ecrire_Ligne(Image(I) &' '& Dico(I).Mot &' '& Image(Dico(I).Score));
    end loop;
 end;
---
+++ pinfo.adb
@@ -0,0 +1,29 @@
+with P_Generation; use P_Generation;
+with P_Classification; use P_Classification;
+with P_Depeche; use P_Depeche, P_Depeche.P_categorieio;
+with P_Esiut; use P_Esiut;
+
+procedure Pinfo is   
+   TVD : TV_Depeche;
+   Dico : TV_Dico(1..2000);
+   N : Integer;
+   Min, Q1, Med, Q3, Max : Integer := 0;
+begin
+   Charge("txt/depeches.txt", TVD);
+   for I in T_Categorie loop
+      N := 0;
+      Init_Dico(TVD, I, Dico, N);
+      Calcul_Scores(TVD, I, Dico, N);
+      
+      A_La_Ligne;
+      Ecrire_Ligne(Image(I) &':');
+      Infos_Scores(Dico, N, Min, Q1, Med, Q3, Max);
+      Ecrire("Min:" & Image(Min) & "  ");
+      Ecrire("Max:" & Image(Max));
+      
+      A_La_Ligne;
+      Ecrire("Q1:" & Image(Q1) & "  ");
+      Ecrire("Med:" & Image(Med) & "  ");
+      Ecrire("Q3:" & Image(Q3));
+   end loop;
+end;
