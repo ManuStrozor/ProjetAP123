ed6dfa9c64d9283a728d78b6ebe5f55ee2c07773
Emmanuel Turbet <m.emmanuel.td@gmail.com>
2019-06-16 18:22:09 +0200

    ameliore de ouf p52

--- p_classification.adb
+++ p_classification.adb
@@ -1,3 +1,5 @@
+with P_String; use P_String;
+
 package body P_Classification is
    
    function Score(D: in TR_Depeche; L : in TV_Lexique) return Integer is
@@ -54,12 +56,40 @@ package body P_Classification is
    
    procedure Run (VD : in TV_Depeche; Lp, Ls, Lc, Le, Lt : TV_Lexique; Nomfic : String; A,B,C,D,E,G : in Integer) is
       -- {} => {Génère le fichier texte réponse Nomfic étant donné les dépêches et les lexiques passés en argument}
+      
+      function Rech(Mot : in String; L : in TV_Lexique) return Integer is
+	 -- {L non trié},=> {résultat = indice si L contient le mot}
+	 Me, Inf, Sup : Integer;
+      begin
+	 if L(L'last).Chaine < Mot then
+	    return L'Last+1;
+	 else
+	    Inf := L'First; Sup := L'Last;
+	    while Inf < Sup loop
+	       Me := (Inf + Sup) / 2 ;
+	       if L(Me).Chaine >= Mot then
+		  Sup := Me;
+	       else
+		  Inf := Me + 1;
+	       end if;
+	    end loop;
+	    if L(Sup).Chaine = Mot then 
+	       return Sup;
+	    else 
+	       return L'Last+1;
+	    end if;
+	 end if;
+      end;
+      
       F : Text_Io.File_Type;
       Sc : TV_Score;
       type TV_Cnt is array(T_Categorie) of Integer;
       Cnt : TV_Cnt := (others => 0);
       Moy : Float := 0.0;
       Mx : T_Categorie;
+      type TV_Files is array(T_Categorie) of String(1..30);
+   
+      Z : TV_Files;
    begin
       Create(F, Out_File, Nomfic);
       for I in VD'Range loop
@@ -73,13 +103,29 @@ package body P_Classification is
 	 if Mx = VD(I).Cat then
 	    Cnt(Mx) := Cnt(Mx) + 1;
 	 else
-	    Put_Line(VD(I).Id &':'& Image(VD(I).Cat) & Integer'Image(Sc(VD(I).Cat)) & " -> " & Image(Mx) & Integer'Image(Sc(Mx)));
-	    --  for J in Sc'Range loop
-	    --     Put(Image(J) &':'& Integer'Image(Sc(J)) &' ');
-	    --  end loop;
-	    --  New_Line;
-	    Affiche(VD(I));
-	    New_Line;
+	    Put_Line(VD(I).Id &":("& Image(VD(I).Cat) &')'& Integer'Image(Sc(VD(I).Cat)) & " -> [" & Image(Mx) &']'& Integer'Image(Sc(Mx)));
+	    for J in VD(I).Texte'First..VD(I).NbMots loop
+	       Z := (uTrim("lex/POLITIQUE-2.txt", 30),
+		     uTrim("lex/SPORTS-2.txt", 30),
+		     uTrim("lex/CULTURE-2.txt", 30),
+		     uTrim("lex/ECONOMIE-2.txt", 30),
+		     uTrim("lex/SCIENCES-2.txt", 30));
+	       declare
+		  L1 : TV_Lexique(1..Nb_Mots(Trim(Z(VD(I).Cat))));
+		  L2 : TV_Lexique(1..Nb_Mots(Trim(Z(Mx))));
+	       begin
+		  Init_Lexique(Trim(Z(VD(I).Cat)), L1);
+		  Init_Lexique(Trim(Z(Mx)), L2);
+		  if Rech(VD(i).Texte(J), L1) /= L1'Last+1 then
+		     Put('(' & Trim(VD(I).Texte(J)) & ") ");
+		  elsif Rech(VD(i).Texte(J), L2) /= L2'Last+1 then
+		     Put('[' & Trim(VD(I).Texte(J)) & "] ");
+		  else
+		     Put(Trim(VD(I).Texte(J)) & ' ');
+		  end if;
+	       end;
+	    end loop;
+	    New_Line(2);
 	 end if;
       end loop;
       for I in T_Categorie loop
